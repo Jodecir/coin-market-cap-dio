180c2eb0b208ff8e84800a9d3d1a865761612141
Jodecir Neto <jodecirneto@gmail.com>
2021-01-09 10:46:34 -0300

    first historical data

--- js/script.js
+++ js/script.js
@@ -23,9 +23,9 @@ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='
           <img src="img/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
           <div class="media-body">
           <h5 class="mt-2">${api.data[i].name}</h5>
-          <p>${api.data[i].symbol}</p>
           </div>
-      </div>
+          <p>${api.data[i].symbol}</p><p>${api.data[i].first_historical_data}</p>
+      </div><br>
  
       `;
 

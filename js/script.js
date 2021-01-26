50df09594ff96086b432b8cb725d9e4fab2cb8f9
Jodecir Neto <jodecirneto@gmail.com>
2021-01-26 08:55:41 -0300

    unused router removed

--- js/script.js
+++ js/script.js
@@ -17,23 +17,23 @@ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='
   for(let i = 0; i < 10; i++){
 
   //Show API information
-     texto = texto + `
-    
-      <div class="media">
-          <img src="img/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
-          <div class="media-body">
-          <h2 class="mt-2">${api.data[i].name}</h2>
-          <p>(${api.data[i].symbol})</p>
-          </div>
-          <div class="media-body">
-          <h5 class="mt-2">First Historical Data</h5>
-          <p>${api.data[i].first_historical_data}</p>
-          </div>
-      </div><br>
- 
-      `;
+    texto = texto + `
+  
+    <div class="media">
+      <img src="img/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+      <div class="media-body">
+        <h2 class="mt-2">${api.data[i].name}</h2>
+        <p>(${api.data[i].symbol})</p>
+      </div>
+      <div class="media-body">
+        <h5 class="mt-2">First Historical Data</h5>
+        <p>${api.data[i].first_historical_data}</p>
+      </div>
+    </div>
 
-      document.getElementById("coins").innerHTML = texto;
+    `;
+
+    document.getElementById("coins").innerHTML = texto;
   }
 })
 .catch((error) => {
